07b62e6f5c0095e65e7ee1d3e2649d52ee9b3f27
EC2 Default User <ec2-user@ip-172-31-58-200.ec2.internal>
2020-07-03 22:54:51 +0000

    Moved resource servers into the repo. Improved deployment script so infrastructure is not deployed when environment variables are missing

--- identity/deploy.sh
+++ identity/deploy.sh
@@ -1,9 +1,17 @@
 stack_name_identity=focusmark-"$deployed_environment"-cf-identity
-stack_name_client_apps=focusmark-"$deployed_environment"-cf-clientapps
 template_file_identity='identity.yaml'
+
+stack_name_resource_servers=focusmark-"$deployed_environment"-cf-resourceservers
+template_file_resource_servers='resource-servers.yaml'
+
+stack_name_client_apps=focusmark-"$deployed_environment"-cf-clientapps
 template_file_client_apps='client-apps.yaml'
 
 # UserPool deployment
 aws cloudformation deploy --template-file $template_file_identity --stack-name $stack_name_identity --parameter-overrides TargetEnvironment=$deployed_environment
+
+# UserPool Resource Servers
+aws cloudformation deploy --template-file $template_file_resource_servers --stack-name $stack_name_resource_servers --parameter-overrides TargetEnvironment=$deployed_environment
+
 # Client Apps deployment
 aws cloudformation deploy --template-file $template_file_client_apps --stack-name $stack_name_client_apps --parameter-overrides TargetEnvironment=$deployed_environment
