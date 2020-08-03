9559e25448be3c21b703aa6df02a7b8c2cd5aa36
Scionwest <scionwest+github@gmail.com>
2020-08-03 02:01:53 +0000

    Removed the resource-servers template as it needs to be part of the new api-infrastructure repo

--- core/deploy.sh
+++ core/deploy.sh
@@ -1,27 +1,29 @@
+# Deploy S3 buckets that are shared across all resources within an environment
 sharedbuckets_template='shared-buckets.yaml'
 sharedbuckets_stackname=focusmark-"$deployed_environment"-cf-sharedbuckets
-
-apidomain_template='api-domain.yaml'
-apidomain_stackname=focusmark-"$deployed_environment"-cf-core
-
-certificates_template='api-certificates.yaml'
-certificates_stackname=focusmark-"$deployed_environment"-cf-apicertificates
-
 echo Deploying the $sharedbuckets_stackname stack.
+
 aws cloudformation deploy \
     --template-file $sharedbuckets_template \
     --stack-name $sharedbuckets_stackname \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides TargetEnvironment=$deployed_environment
     
+# Deploy certificates
+certificates_template='api-certificates.yaml'
+certificates_stackname=focusmark-"$deployed_environment"-cf-apicertificates
 echo Deploying the $certificates_stackname stack.
+
 aws cloudformation deploy \
     --template-file $certificates_template \
     --stack-name $certificates_stackname \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides TargetEnvironment=$deployed_environment
 
-echo Deploying the apidomain_stackname stack.
+# Deploy the API Gateway Domain
+apidomain_template='api-domain.yaml'
+apidomain_stackname=focusmark-"$deployed_environment"-cf-core
+echo Deploying the $apidomain_stackname stack.
 aws cloudformation deploy \
     --template-file $apidomain_template \
     --stack-name $apidomain_stackname \
--- identity/deploy.sh
+++ identity/deploy.sh
@@ -1,20 +1,13 @@
+# UserPool deployment
 stack_name_identity=focusmark-"$deployed_environment"-cf-identity
 template_file_identity='identity.yaml'
 
-stack_name_resource_servers=focusmark-"$deployed_environment"-cf-resourceservers
-template_file_resource_servers='resource-servers.yaml'
-
-stack_name_client_apps=focusmark-"$deployed_environment"-cf-clientapps
-template_file_client_apps='client-apps.yaml'
-
-# UserPool deployment
 echo Deploying the $stack_name_identity stack.
 aws cloudformation deploy --template-file $template_file_identity --stack-name $stack_name_identity --parameter-overrides TargetEnvironment=$deployed_environment
 
-# UserPool Resource Servers
-echo Deploying the $stack_name_resource_servers stack.
-aws cloudformation deploy --template-file $template_file_resource_servers --stack-name $stack_name_resource_servers --parameter-overrides TargetEnvironment=$deployed_environment
-
 # Client Apps deployment
+stack_name_client_apps=focusmark-"$deployed_environment"-cf-clientapps
+template_file_client_apps='client-apps.yaml'
+
 echo Deploying the $stack_name_client_apps stack.
 aws cloudformation deploy --template-file $template_file_client_apps --stack-name $stack_name_client_apps --parameter-overrides TargetEnvironment=$deployed_environment
