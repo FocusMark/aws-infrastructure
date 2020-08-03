c6545d583a916cd5badae9b846939a30bf9cde1b
Scionwest <scionwest+github@gmail.com>
2020-08-03 04:10:42 +0000

    Moved dns and oauth templates back to this repo due to dependencies. Began WIP of DNS wire up via Route 53

--- core/deploy.sh
+++ core/deploy.sh
@@ -1,3 +1,5 @@
+product_name=$focusmark_productname
+
 # Deploy S3 buckets that are shared across all resources within an environment
 sharedbuckets_template='shared-buckets.yaml'
 sharedbuckets_stackname=focusmark-"$deployed_environment"-cf-sharedbuckets
@@ -7,7 +9,22 @@ aws cloudformation deploy \
     --template-file $sharedbuckets_template \
     --stack-name $sharedbuckets_stackname \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides TargetEnvironment=$deployed_environment
+    --parameter-overrides \
+        TargetEnvironment=$deployed_environment \
+        ProductName=$product_name
+    
+# Deploy the Route 53 Hosted Zones - MUST run before certificates so that the needed CNAME records can be created when the Certificate template runs.
+hostedzone_template='hosted-zones.yaml'
+hostedzone_stackname=focusmark-"$deployed_environment"-cf-hostedzones
+
+echo Deploying the $hostedzone_stackname stack.
+aws cloudformation deploy \
+    --template-file $hostedzone_template \
+    --stack-name $hostedzone_stackname \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+        TargetEnvironment=$deployed_environment \
+        ProductName=$product_name
     
 # Deploy certificates
 certificates_template='api-certificates.yaml'
@@ -18,14 +35,18 @@ aws cloudformation deploy \
     --template-file $certificates_template \
     --stack-name $certificates_stackname \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides TargetEnvironment=$deployed_environment
+    --parameter-overrides \
+        TargetEnvironment=$deployed_environment \
+        ProductName=$product_name
 
 # Deploy the API Gateway Domain
 apidomain_template='api-domain.yaml'
-apidomain_stackname=focusmark-"$deployed_environment"-cf-core
+apidomain_stackname=focusmark-"$deployed_environment"-cf-apidomain
 echo Deploying the $apidomain_stackname stack.
 aws cloudformation deploy \
     --template-file $apidomain_template \
     --stack-name $apidomain_stackname \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides TargetEnvironment=$deployed_environment
+    --parameter-overrides \
+        TargetEnvironment=$deployed_environment \
+        ProductName=$product_name
