827bd1e50975176aa02320ce20a272f55b4a5044
Scionwest <scionwest+github@gmail.com>
2020-08-16 02:00:38 +0000

    Fixed godaddy nameserver resolver lambda and custom resource timing out after an hour and never deploying due to an IAM policy issue

--- core/hostedzone_nameserver_resolver/src/godaddy-request.js
+++ core/hostedzone_nameserver_resolver/src/godaddy-request.js
@@ -6,7 +6,8 @@ class GoDaddyRequest {
         this.secret = secret;
     }
     
-    async setNameservers(nameservers) {
+    setNameservers(nameservers) {
+        console.info('Building the Nameservers list');
         let newNameservers = nameservers.map(server => {
             return {
                 type: 'NS',
@@ -20,15 +21,13 @@ class GoDaddyRequest {
             timeout: 60000
         });
         
-        try {
-            // AWS naming conventions for their nameservers: https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/SOA-NSrecords.html
-            //let t1 = await instance.get(this.apiUrl);
-            //const response = await instance.put(this.apiUrl, newNameservers);
-            //console.info(response.data);
-        } catch(err) {
-            console.info(err.response.data);
-        }
+        console.info('Setting nameservers');
+        // AWS naming conventions for their nameservers: https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/SOA-NSrecords.html
+        //let t1 = await instance.get(this.apiUrl);
+        //const response = await instance.put(this.apiUrl, newNameservers);
+        //console.info(response.data);
         
+        console.info('Nameserver setting completed');
     }
 }
 
--- core/hostedzone_nameserver_resolver/src/handler.js
+++ core/hostedzone_nameserver_resolver/src/handler.js
@@ -13,63 +13,101 @@ let route53 = new AWS.Route53();
 let systemManager = new AWS.SSM();
 let secretsManager = new AWS.SecretsManager();
 
-exports.run = async (event, context) => {
-    try {
-        let config = new Configuration();
-        console.info(event);
-        console.info(config);
-        let request = new CloudFormationRequest(event);
+// async function test(event,context) {
+//     return new Promise(function(resolve,reject) {
+//         response.send(event, context, "SUCCESS");
+//         resolve();
+//     });
+// }
+
+exports.run = (event, context) => {
+    console.info('Starting Nameserver resolver process')
+    let config = new Configuration();
+    console.info(event);
+    console.info(config);
+    let request = new CloudFormationRequest(event);
+    
+    // Do not process any request types other than Create
+    if (request.requestType !== 'Create') {
+        console.info('Resolver executed but CloudFormation is not creating new resources. Resolver will exit.');
+        response.send(event, context, "SUCCESS")
+        return;
+    } // TODO: Handle 'Delete' by setting the GoDaddy Nameservers back to their default if the API allows for it
+    
+    console.info('Executing request processing');
+    processRequest(request, config, function(err) {
+        console.info('Processing wrapped up');
+        if (err) {
+            console.info('Processing concluded with an error');
+            console.info(err);
+            console.info('Aborting Lambda execution');
+            response.send(event, context, "FAILED");
+            return;                
+        }
         
-        // Do not process any request types other than Create
-        if (request.requestType !== 'Create') {
-            response.send(event, context, "SUCCESS")
+        console.info('Resolver completed successfullly');
+        response.send(event, context, "SUCCESS");
+    });
+};
+
+function processRequest(request, config, callback) {
+    getHostedZone(request.hostedZoneId, function(hostedZone, hostedZoneError) {
+        if (hostedZoneError) {
+            callback(hostedZoneError);
             return;
         }
         
-        await processRequest(request);
-        response.send(event, context, "SUCCESS");
-    } catch(err) {
-        console.info(err);
-        console.info('Aborting Lambda execution');
-        response.send(event, context, 'FAILED');
-        return;
-    }
-};
-
-async function processRequest(request, config) {
-    let hostedZone = await getHostedZone(request.hostedZoneId);
-    let godaddyInfo = await getGodaddyInfo(config);
-    
-    let godaddyRequest = new GoDaddyRequest(godaddyInfo.apiUrl, godaddyInfo.secret);
-    await godaddyRequest.setNameservers(hostedZone.nameservers);
+        console.info('Nameserver data retrieved');
+        console.info(hostedZone);
+        
+        getGodaddyInfo(config, function(godaddyInfo, godaddyError) {
+            if (godaddyError) {
+                console.info('Failed to retrieve all of the GoDaddy information needed to process the Nameserver resolver');
+                callback(godaddyError);
+                return;
+            }
+            
+            console.info('Preparing the API call to GoDaddy');
+            let godaddyRequest = new GoDaddyRequest(godaddyInfo.apiUrl, godaddyInfo.secret);
+            godaddyRequest.setNameservers(hostedZone.nameservers);
+            
+            console.info('Request processing completed');
+            callback();
+        });
+    });
+ 
 }
 
-async function getHostedZone(hostedZoneId) {
-    let ssmParams = {
-        Name: hostedZoneId
-    };
-    
-    let ssmResult = await systemManager.getParameter(ssmParams).promise();
-    
+function getHostedZone(hostedZoneId, callback) {
+    console.info('Fetching the Route 53 Hosted Zone and the Nameservers associated with it.');
     let route53Params = {
-        Id: ssmResult.Parameter.Value
+        Id: hostedZoneId
     };
 
-    let route53Result = await route53.getHostedZone(route53Params).promise();
-    let hostedZone = {
-        hostedZoneId: route53Params.Id,
-        name: route53Result.HostedZone.Name,
-        nameservers: route53Result.DelegationSet.NameServers,
-    };
-    
-    if (hostedZone.name.endsWith('.')) {
-        hostedZone.name = hostedZone.name.substring(0, hostedZone.name.length - 1);
-    }
-    
-    return hostedZone;
+    route53.getHostedZone(route53Params, function(err, route53Result) {
+        if (err) {
+            console.info('Failed to retrieve nameservers from Route 53');
+            callback(null, err);
+            return;
+        }
+        
+        console.info('Building hosted zone and nameserver results object');
+        let hostedZone = {
+            hostedZoneId: route53Params.Id,
+            name: route53Result.HostedZone.Name,
+            nameservers: route53Result.DelegationSet.NameServers,
+        };
+        
+        if (hostedZone.name.endsWith('.')) {
+            hostedZone.name = hostedZone.name.substring(0, hostedZone.name.length - 1);
+        }
+
+        callback(hostedZone);
+    });
 }
 
-async function getGodaddyInfo(config) {
+async function getGodaddyInfo(config, callback) {
+    console.info('Fetching GoDaddy information');
     let ssmParams = {
         Name: config.registrarApiParameter
     };
@@ -77,11 +115,28 @@ async function getGodaddyInfo(config) {
         SecretId: config.registrarSecretParameter
     };
     
-    let ssmResult = await systemManager.getParameter(ssmParams).promise();
-    let secretResult = await secretsManager.getSecretValue(secretParams).promise();
-    
-    return {
-        secret: secretResult.SecretString,
-        apiUrl: ssmResult.Parameter.Value
-    };
+    console.info('Looking up the GoDaddy API Parameter from SSM');
+    systemManager.getParameter(ssmParams, function(ssmError, ssmResult) {
+        if (ssmError) {
+            console.info('Failed to find the API parameter in SSM');
+            callback(null, ssmError);
+            return;
+        }
+        
+        console.info('Parameter discovered.');
+        console.info('Looking up the GoDaddy API Key and Secret from Secret Manager');
+        secretsManager.getSecretValue(secretParams, function(secretError, secretResult) {
+            if (secretError) {
+                console.info('Failed to find the Key and Secret');
+                callback(null, secretError);
+                return;
+            }
+            
+            console.info('Key and Secret discovered');
+            callback({
+                secret: secretResult.SecretString,
+                apiUrl: ssmResult.Parameter.Value
+            });
+        });
+    });
 }
