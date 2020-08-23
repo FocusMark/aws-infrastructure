31e634bd0456ed35e751aa9bd2ba7c006b7c56e5
Scionwest <scionwest+github@gmail.com>
2020-08-23 19:58:56 +0000

    Completed refactor of repository

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,12 @@ fi
 echo Deploying into the $deployed_environment environment.
 
 echo Deploying core cross-cutting infrastructure
-cd core
+cd src/core
 sh deploy.sh
-cd ..
+cd ../..
 
 # Hosted Zone must be deployed before anything further  as everything after it depends on the certificates and DNS that is built on-top of the Hosted Zone.
 echo Deploying Hosted Zone for domain management
-cd hostedzone
+cd src/hostedzone
 sh deploy.sh
-cd ..
-
-echo Deploying identity services
-cd ../identity
-sh deploy.sh
-cd ..
+cd ../..
